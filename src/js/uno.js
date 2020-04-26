dd2786ba9a9f68350aa059ba8ce207e17f80e7d8
nkwazi <janekdekock@protonmail.com>
2020-04-26 14:58:31 +0200

    Adds playing functions & visualization

--- src/js/uno.js
+++ src/js/uno.js
@@ -1,15 +1,16 @@
-var cards = new Array(108), players = new Array(5);
+'use strict';
+var cards = [];
+var dropped_cards = [];
+var players = [];
 
 function new_game() {
-  make_deck()
-  shuffle(cards)
-  show_players()
-}
-
-function Player(name, card1, card2) {
-  this.name = name;
-  this.card1 = card1;
-  this.card2 = card2;
+  make_deck();
+  dropped_cards = [];
+  update_game_text('stack', "");
+  shuffle(cards);
+  generate_players(3);
+  show_players();
+  heap_count();
 }
 
 function Card(number, colour) {
@@ -17,32 +18,78 @@ function Card(number, colour) {
   this.colour = colour;
 }
 
-function show_players() {
-  str = "";
-  for (i = 0; i < 5; i++) {
-    players[i] = new Player("Player " + i, "", "");
-    str += players[i].name + "\n";
+function drop_allowed(card) {
+  // TODO: check if card can be dropped  
+}
+
+function generate_players(no_of_players) {
+  for (var i = 0; i < no_of_players; i++) {
+    players[i] = {
+      name: "Player " + i,
+      pl_cards: []
+    }
   }
-  update_game_text('game', str);
-  update_game_text('stack', "")
+}
+
+function get_cards(p) {
+  var cards_str = "";
+  var no_of_cards = players[p].pl_cards.length;
+  for (let j = 0; j < no_of_cards; j++) {
+    cards_str += players[p].pl_cards[j].number + " " + players[p].pl_cards[j].colour + "\t";
+  }
+  return cards_str;
+}
+
+function show_players() {
+  var player_str = "";
+  var numb_of_players = players.length;
+  for (let k = 0; k < numb_of_players; k++) {
+    player_str += players[k].name + "\t" + get_cards(k) + "\n";
+  }
+  update_game_text('game', player_str);
 }
 
 function start_game() {
-  top_card = cards.pop();
-  text = top_card.colour + " " + top_card.number
-  update_game_text('stack', text)
+  var top_card = cards.pop();
+  var text = top_card.colour + " " + top_card.number;
+  dropped_cards.push(top_card);
+  update_game_text('stack', text);
+  heap_count();
 }
 
-function deal_cards() {
-  str = "";
-  for (i = 0; i < 5; i++) {
-    card1 = cards.pop();
-    card2 = cards.pop();
-    players[i] = new Player("Player " + i, card1, card2);
-    str += players[i].name + "\t" + players[i].card1.number + " " + players[i].card1.colour
-      + "\t" + players[i].card2.number + " " + players[i].card1.colour + "\n";
+function draw_card() {
+  var p = prompt('Enter player number');
+  if (cards.length > 0) {
+    var new_card = cards.pop();
   }
-  update_game_text('game', str);
+  players[p].pl_cards.push(new_card);
+  show_players();
+}
+
+function drop_card() {
+  var p = prompt('Enter player number');
+  if (players[p].pl_cards.length > 0) {
+    var card = players[p].pl_cards.pop();
+  }
+  else if (players[p].pl_cards.length == 1) {
+    won();
+  }
+  update_game_text('stack', card);
+  dropped_cards.push(card);
+  show_players();
+}
+
+function deal_cards(nmb_of_cards) {
+  for (let i = 0; i < players.length; i++) {
+    var j = 0;
+    while (j < nmb_of_cards) {
+      var popped_card = cards.pop();
+      players[i].pl_cards.push(popped_card);
+      j++;
+    }
+  }
+  show_players();
+  heap_count();
 }
 
 function make_deck() {
@@ -110,6 +157,9 @@ function make_deck() {
   }
 }
 
+function won() {
+  alert('Game over');
+}
 // Fisher-Yates (aka Knuth) Shuffle
 function shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex;
@@ -133,15 +183,20 @@ function shuffle(array) {
 function print_cards() {
   var i = 0;
   var str = "Cards: ";
-  while (i < 108) {
+  while (i < cards.length) {
     str += "\n";
-    str += cards[i];
+    str += cards[i].colour + " " + cards[i].number;
     i++;
   }
-  update_game_text(str);
+  update_game_text('game', str);
 }
 
 function update_game_text(id, text) {
   game = document.getElementById(id);
   game.textContent = text;
+}
+
+function heap_count() {
+  update_game_text('heap', "Heap count: " + cards.length);
+  update_game_text('dropped_cards', "Dropped cards: " + dropped_cards.length);
 }
