c0bf59fa3db0c029a91151e5b3ebd5995065ce07
Luka Vucenovic <luka.vucenovic96@gmail.com>
2020-05-03 11:06:15 +0200

    Interface for the Card Game, with basic functionallities

---
+++ src/CardGame.sol
@@ -0,0 +1,15 @@
+pragma solidity >=0.4.16 <0.7.0;
+
+interface CardGame {
+    
+
+    function changeCardSuit(uint16 _suit) external returns (uint8);
+
+    function revealCardValue(uint16 _index) external view returns(uint16);
+
+    function revealCardSuit(uint16 _index) external view returns(string);
+
+    function revealFullCard(uint16 _index) external view returns(uint16,uint16);
+
+    function commitCard(uint16 _card_committed, uint16 _quantity) external;
+}
