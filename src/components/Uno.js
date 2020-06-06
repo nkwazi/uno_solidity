27c60c2b7f405f3e7c5481aee486e8cc49a0e335
nkwazi <janekdekock@protonmail.com>
2020-06-06 19:34:47 +0200

    current changes

--- src/components/Uno.js
+++ src/components/Uno.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Game, Color, Value } from 'uno-engine';
+import UnoSOL from '../abis/contracts/Uno.json';
 import Web3 from 'web3';
 
 export default class Uno extends Component {
@@ -14,12 +15,32 @@ export default class Uno extends Component {
       name: '',
       mainHand: [],
       account: '',
+      unoSOL: null,
       gameReady: false,
       gameState: 'Waiting for players...',
+      gameOver: false
     };
   }
 
-  componentDidMount() {
+  async componentDidMount() {
+    this.loadWeb3()
+    this.loadBlockchain()
+    this.initGame()
+  }
+
+  initGame() {
+    this.setState({ name: this.props.name})
+    this.setState({ players: [this.props.name, this._player2] })
+    this.pushPlayer(this.props.name)
+    this.pushPlayer(this._player2)
+    const newGame = new Game(this.state.players)
+    console.log(newGame.deck)
+    const p = newGame.getPlayer(this.props.name)
+    this.setState({ game: newGame })
+    this.prepareHand(p)
+  }
+
+  loadWeb3() {
     if (typeof window.ethereum !== 'undefined') {
       window.ethereum.enable();
       const web3 = new Web3(window.ethereum);
@@ -33,21 +54,81 @@ export default class Uno extends Component {
           this.isGameReady()
           this.setState({ account: accounts[0] })
           console.log('Connected');
+          console.log(accounts)
         }
       });
     } else {
       this.couldNotConnect()
       console.log('Unavailable');
     }
-    this.setState({ name: this.props.name})
-    this.setState({ players: [this.props.name, this._player2] })
-    this.pushPlayer(this.props.name)
-    this.pushPlayer(this._player2)
-    const newGame = new Game(this.state.players)
-    console.log(newGame.deck)
-    const p = newGame.getPlayer(this.props.name)
-    this.setState({ game: newGame })
-    this.prepareHand(p)
+  }
+
+  loadBlockchain() {
+    window.ethereum.enable();
+    const web3 = new Web3(window.ethereum);
+    this.setState({ web3 })
+    const accounts = web3.eth.getAccounts((err, accounts) => {
+      if(err) {
+        throw err;
+      }
+      else {
+        console.log('Accounts: ', accounts)
+        let networkId = web3.eth.net.getId((err, id) => {
+          console.log('ID', id)
+          const networkData = UnoSOL.networks[5777]
+          console.log('Network', networkData)
+          const unoSOL = new web3.eth.Contract(UnoSOL.abi, networkData.address)
+          this.setState({ unoSOL })
+          console.log('SUCCESS!!!', networkData.address)
+        })
+      }
+    })
+
+    // const networkId = web3.eth.net.getId((err, id) => {
+    //   if(err) {
+    //     throw err;
+    //   }
+    //   else {
+    //     console.log('ID: ', id)
+    //     return id
+    //   }
+    // })
+
+    // this.setState({ account: accounts[0] })
+    // // Network ID
+    // const networkId = await web3.eth.net.getId()
+    // const networkData = UnoSOL.networks[networkId]
+    // if(networkData) {
+    //   const unoSOL = web3.eth.Contract(UnoSOL.abi, networkData.address)
+    //   this.setState({ unoSOL })
+    //   const postCount = await unoSOL.methods.postCount().call()
+    //   this.setState({ postCount })
+    //   // Load Posts
+    //   for (var i = 1; i <= postCount; i++) {
+    //     const post = await unoSOL.methods.posts(i).call()
+    //     this.setState({
+    //       posts: [...this.state.posts, post]
+    //     })
+    //   }
+    //   // Sort posts. Show highest tipped posts first
+    //   this.setState({
+    //     posts: this.state.posts.sort((a,b) => b.tipAmount - a.tipAmount )
+    //   })
+    //   this.setState({ loading: false})
+    // } else {
+    //   window.alert('UnoSOL contract not deployed to detected network.')
+    // }
+  }
+
+  chooseWinner(address) {
+    let val = this.state.unoSOL.methods.chooseWinner(address).send({ 'from': '0x1DC6A6882c9C1B380EBc206180C735a34aeED553', 'gas': '1000000', 'value': this.state.web3.utils.toWei('0.5', 'ether') })
+    console.log('Winner', val)
+  }
+
+  joinGame() {
+    let val = this.state.unoSOL.methods.joinGame().call((err, ok) => {
+      console.log('Joined Game', ok)
+    })
   }
 
   // update gameState to indicate Metamask connection failed
@@ -85,6 +166,7 @@ export default class Uno extends Component {
     } catch (e) {
       alert('notPossible' + e)
     }
+    this.setState({ gameOver: false })
   }
 
   getCardColor(card) {
@@ -129,8 +211,14 @@ export default class Uno extends Component {
     return img
   }
 
+  drawCard() {
+    this.state.game.draw()
+    this.setState({ gameOver: true })
+    console.log('Game state:', this.state.gameOver)
+  }
+
   render() {
-    if(this.gameReady) {
+    if(this.gameReady && !this.gameOver) {
       return(
         <div className='Game'>
         <div className='aiPlayer'>
@@ -140,11 +228,13 @@ export default class Uno extends Component {
           <div className='image'>
             <p>Deck</p>
             <p>Current Player: {this.state.game.currentPlayer.name} </p>
-            <img src='/images/back.png' className='image' alt='foob' />
+            <img src='/images/back.png' className='image' alt='foob' onClick={() => this.state.game.draw()} />
             { this.displayCard(this.state.game.discardedCard) }
-            <button onClick={() => this.state.game.draw()}>Draw</button>
+            <button onClick={() => this.drawCard()}>Draw</button>
+            <button onClick={() => this.joinGame()}>Join</button>
             <button onClick={() => this.state.game.pass()}>Pass</button>
             <button onClick={() => this.updateUI()}>Update</button>
+            <button onClick={() => this.chooseWinner(this.state.account)}>Winner</button>
           </div>
           <div className='player'>
             <p> {this.state.name} [ {this.state.account} ] </p>
@@ -155,6 +245,13 @@ export default class Uno extends Component {
               }
             </div>
           </div>
+        </div>
+      )
+    }
+    if (this.gameOver) {
+      return(
+        <div>
+          <p>Winner winner chicken dinner</p>
         </div>
       )
     }
