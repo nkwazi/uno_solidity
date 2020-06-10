9ea8b4c8f72c15ab5816a34e885c20b885e14808
nkwazi <janekdekock@protonmail.com>
2020-06-10 18:17:57 +0200

    Working version need to improve UI

--- src/components/Uno.js
+++ src/components/Uno.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import { Game } from 'uno-engine';
 import UnoSOL from '../abis/contracts/Uno.json';
 import Web3 from 'web3';
+import Account from './Account';
 
 export default class Uno extends Component {
 
-  _player2 = 'Player 2';
-  _canGoAgain = true;
+  _player2 = 'Player 2'
+  _canGoAgain = true
+  _i = 0
 
   constructor(props) {
     super(props);
@@ -16,6 +18,7 @@ export default class Uno extends Component {
       name: '',
       mainHand: [],
       account: '',
+      balance: 0,
       unoSOL: null,
       gameReady: false,
       gameState: 'Waiting for players...',
@@ -27,6 +30,9 @@ export default class Uno extends Component {
     this.loadWeb3()
     this.loadBlockchain()
     this.initGame()
+    while(!this.state.gameOver && this.state.game.currentPlayer === this._player2) {
+      this.aiPlayer()
+    }
   }
 
   initGame() {
@@ -88,11 +94,29 @@ export default class Uno extends Component {
   chooseWinner(address) {
     let val = this.state.unoSOL.methods.chooseWinner(address).send({ 'from': this.state.account, 'gas': '1000000', 'value': this.state.web3.utils.toWei('0.5', 'ether') })
     console.log('Winner', val)
+    this.state.web3.eth.getBalance(this.state.account, (err, balance, e) => {
+      if (balance) {
+        if (typeof balance == 'string') {
+          balance = parseFloat(balance) / 1000000000000000000;
+        } else {
+          balance = balance.toNumber() / 1000000000000000000;
+        }
+        this.setState({ balance: balance });
+      }
+    })
   }
 
+  // TODO find place to use
   joinGame() {
     this.state.unoSOL.methods.joinGame().call((err, ok) => {
       console.log('Joined Game', ok)
+    })
+  }
+
+  // TODO find place to use
+  startGame() {
+    this.state.unoSOL.methods.startGame().call(() => {
+      console.log('Game started')
     })
   }
 
@@ -112,7 +136,7 @@ export default class Uno extends Component {
   displayCard(cardToDisplay) {
     console.log('Card to display:' + cardToDisplay)
     const currentCard = '/images/' + this.cardToImageConverter(cardToDisplay) +'.png';
-    return( <img src={currentCard} alt='currentCard'/>)
+    return(<img src={currentCard} alt='currentCard'/>)
   }
 
   getNumber(input) {
@@ -131,19 +155,34 @@ export default class Uno extends Component {
     }
   }
 
-  // TODO +2 should change player
   throwCard(_card) {
     try {
       if (_card.color === undefined) {
         let colour = prompt('Please enter a color')
         _card.color = this.getNumber(colour)
       }
+      if (this.state.game.currentPlayer.hand.length === 1) {
+        this.setState({ gameOver: true })
+        console.log(this.state.game.currentPlayer.hand.length)
+      }
       this.state.game.play(_card)
     } catch (e) {
       console.log(e)
+      this.forceUpdate()
     } finally {
+      if (this.state.gameOver) {
+        this.chooseWinner(this.state.account)
+        alert('Game Over')
+        this.forceUpdate()
+      }
       this.forceUpdate()
     }
+  }
+
+  getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
   checkAllCards(player) {
@@ -151,24 +190,31 @@ export default class Uno extends Component {
       if(this.state.game.discardedCard.color === player.hand[i].color || this.state.game.discardedCard.value === player.hand[i].value) {
         this.throwCard(player.hand[i])
         break
+      }
+      else if(player.hand[i].color === undefined) {
+        player.hand[i].color = this.getRandomInt(1, 4)
+        this.throwCard(player.hand[i])
+        break
       } else {
-        console.log(player.hand[i].name + ' ' + player.hand[i].value)
+        console.log(player.hand[i].color + ' ' + player.hand[i].value)
+        break
       }
     }
+    //this.drawCard()
+    //this.pass()
   }
 
   aiPlayer() {
-    const player = this.state.game.currentPlayer
-    this._canGoAgain = true
+    const player = this.state.game.getPlayer(this._player2)
     console.log('AI starting...', player.name)
     if(player.name === this._player2) {
-      this.checkAllCards(player)
-    } else if (this._canGoAgain) {
-      this.drawCard()
-      this.checkAllCards(player)
-    } else {
-      this.pass()
-      console.log('Not my turn')
+      try {
+        this.checkAllCards(player)
+      } catch (e) {
+        console.log(e)
+      } finally {
+        this.forceUpdate()
+      }
     }
   }
 
@@ -198,10 +244,11 @@ export default class Uno extends Component {
     else if (_card.value === 10) {
       img = this.getCardColor(_card) + '_+2'
     }
-    else if (_card.value === 11) {
+    else if (_card.value === 12) {
       img = this.getCardColor(_card) + '_skip'
     }
-    else if (_card.value === 12) {
+    else if (_card.value === 11) {
+      //this.state.game.setNextPlayer()
       img = this.getCardColor(_card) + '_reverse'
     }
     else if (_card.value === 13) {
@@ -216,7 +263,6 @@ export default class Uno extends Component {
   skipPlayer2() {
     let newCard = this.state.game.draw()
     console.log('Picked Up', newCard)
-    //this.state.mainHand.push(newCard)
     this.state.game.pass()
     this.forceUpdate()
   }
@@ -230,47 +276,56 @@ export default class Uno extends Component {
   }
 
   pass()  {
-    this.state.game.pass()
-    this.forceUpdate()
+    try {
+      this.state.game.pass()
+    } catch (e) {
+      console.log(e);
+    } finally {
+      this.forceUpdate()
+    }
   }
 
   render() {
+    if (this.gameOver) {
+      return(
+        <div>
+          <p>Winner winner chicken dinner</p>
+        </div>
+      )
+    }
     if(this.gameReady && !this.gameOver) {
       return(
         <div className='Game'>
         <div className='aiPlayer'>
           <div className='images' alt='foo'>
             { this.state.game.getPlayer(this._player2).hand.map(images => {
-              return( <img key={images} src={ ('/images/' + this.cardToImageConverter(images) +'.png') } onClick={() => this.throwCard(images)} alt='aiCard'/>)
+              return( <img key={this._i++} src={ ('/images/' + this.cardToImageConverter(images) +'.png') } onClick={() => this.throwCard(images)} alt='aiCard'/>)
               })
             }
           </div>
         </div>
+          <div className='Account'>
+            <Account name={this.state.name} account={this.state.account} />
+          </div>
           <div className='image'>
-            <p>Deck</p>
             <p>Current Player: {this.state.game.currentPlayer.name} </p>
+            <p>Current Color: {this.getCardColor(this.state.game.discardedCard)} </p>
             <img src='/images/back.png' className='image' alt='foob' onClick={() => this.drawCard()} />
             { this.displayCard(this.state.game.discardedCard) }
             <button onClick={() => this.pass()}>Pass</button>
+            <button onClick={() => this.aiPlayer()}>Play AI</button>
             <button onClick={() => console.log(this.state.game.getPlayer(this.props.name).hand)}>Show real Hand</button>
             <button onClick={() => this.chooseWinner(this.state.account)}>Winner</button>
           </div>
           <div className='player'>
             <p> {this.state.name} [ {this.state.account} ] </p>
             <div className='images' alt='foo'>
-              { this.state.game.getPlayer(this.props.name).hand.map((images, element) => {
-                return( <img key={element} src={ ('/images/' + this.cardToImageConverter(images) +'.png') } onClick={() => this.throwCard(images)} alt='playersCard'/>)
+              { this.state.game.getPlayer(this.props.name).hand.map(images => {
+                return( <img key={this._i++} src={ ('/images/' + this.cardToImageConverter(images) +'.png') } onClick={() => this.throwCard(images)} alt='playersCard'/>)
                 })
               }
             </div>
           </div>
-        </div>
-      )
-    }
-    if (this.gameOver) {
-      return(
-        <div>
-          <p>Winner winner chicken dinner</p>
         </div>
       )
     }
